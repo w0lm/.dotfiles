736ee773491e4ff501f3d6648a633a76d1b7c0e1
w0lm <191809111+w0lm@users.noreply.github.com>
2024-12-15 15:44:44 -0800

    wezterm added

---
+++ .wezterm.lua
@@ -0,0 +1,68 @@
+-- Pull wezterm API
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+local act = wezterm.action
+-- Hold Config
+local config = wezterm.config_builder()
+--Config
+config.color_scheme = 'Kanagawa (Gogh)'
+config.font = wezterm.font('Fragment Mono')
+config.font_size = 15
+--Window Settings
+config.window_background_opacity = 0.9
+config.window_decorations = "TITLE|RESIZE"
+config.initial_rows = 26
+config.initial_cols = 110
+--Inactive Pane
+config.inactive_pane_hsb = {
+  saturation = 0.8,
+  brightness = 0.7
+ }
+--Keybindings
+
+config.keys = {
+  { key = 'l', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(1) },
+  { key = 'h', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(-1) },
+  { key = 'j', mods = 'CTRL', action = act.ActivatePaneDirection 'Down', },
+  { key = 'k', mods = 'CTRL', action = act.ActivatePaneDirection 'Up', },
+  { key = 'Enter', mods = 'CTRL', action = act.ActivateCopyMode },
+  { key = 'R', mods = 'SHIFT|CTRL', action = act.ReloadConfiguration },
+  { key = '+', mods = 'CTRL', action = act.IncreaseFontSize },
+  { key = '-', mods = 'CTRL', action = act.DecreaseFontSize },
+  { key = '0', mods = 'CTRL', action = act.ResetFontSize },
+  { key = 'C', mods = 'SHIFT|CTRL', action = act.CopyTo 'Clipboard' },
+  { key = 'N', mods = 'SHIFT|CTRL', action = act.SpawnWindow },
+  { key = 'U', mods = 'SHIFT|CTRL', action = act.CharSelect{ copy_on_select = true, copy_to =  'ClipboardAndPrimarySelection' } },
+  { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+  { key = 'PageUp', mods = 'CTRL', action = act.ActivateTabRelative(-1) },
+  { key = 'PageDown', mods = 'CTRL', action = act.ActivateTabRelative(1) },
+  { key = 'LeftArrow', mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Left' },
+  { key = 'RightArrow', mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Right' },
+  { key = 'UpArrow', mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Up' },
+  { key = 'DownArrow', mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Down' },
+  { key = 'f', mods = 'CTRL', action = act.SplitVertical { domain = 'CurrentPaneDomain' }, },
+  { key = 'd', mods = 'CTRL', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
+  { key = 'h', mods = 'CTRL', action = act.ActivatePaneDirection 'Left', },
+  { key = 'l', mods = 'CTRL', action = act.ActivatePaneDirection 'Right', },
+  { key = 't', mods = 'CTRL', action = act.SpawnTab 'CurrentPaneDomain' },
+  { key = 'w', mods = 'CTRL', action = act.CloseCurrentTab{ confirm = false } },
+  { key = 'x', mods = 'CTRL', action = act.CloseCurrentPane{ confirm = false } },
+  { key = 'b', mods = 'LEADER|CTRL', action = act.SendString '\x02', },
+  { key = 'Enter', mods = 'LEADER', action = act.ActivateCopyMode, },
+  { key = 'k', mods = 'CTRL|ALT', action = act.Multiple
+    {
+      act.ClearScrollback 'ScrollbackAndViewport',
+      act.SendKey { key = 'L', mods = 'CTRL' }
+    },
+  },
+  { key = 'r', mods = 'LEADER', action = act.ActivateKeyTable { name = 'resize_pane', one_shot = false, }, },
+}
+
+--statup config
+wezterm.on('gui-startup',function()
+  local tab, pane, window = mux.spawn_window({})
+  window:gui_window()
+end)
+
+--Return config
+return config
