6e0f36e2f1928a1ec764b6e3dd5a60d3405978d0
w0lm <191809111+w0lm@users.noreply.github.com>
2025-02-09 22:27:00 -0800

    [feat]: flexoki to foot term, neovim plug update

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  --add sonokai
+  { "sainnhe/sonokai" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      sonokai_style = "maia",
+      sonokai_transparent_background = 1,
+      colorscheme = "sonokai",
+    },
+  },
+}
